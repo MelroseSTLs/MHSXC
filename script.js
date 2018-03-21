5cfbc854ec488682172afb6c1f5f53f2daf881d1
jmurphy192 <32463365+jmurphy192@users.noreply.github.com>
2018-03-21 13:39:50 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+// Async HTTP GET Function
+var HttpClient = function() { // Thanks http://stackoverflow.com/a/22076667/1709894!
+  this.get = function(aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function() {
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+        aCallback(anHttpRequest.responseText);
+    }
+
+    anHttpRequest.open( "GET", aUrl, true );
+    anHttpRequest.send( null );
+  }
+}
+
+var getCsvSheet = new HttpClient();
+getCsvSheet.get("https://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheet/ccc?key=1S4dX2zBJ0yoEeJ88SOzb4O9Jxxj-TKMZYtxy45dCTsM&output=csv", function(response) {
+	// Parse local CSV file
+	Papa.parse(response, {
+		complete: function(results) {
+			console.log("Finished:", results.data);
+			var myString = '';
+			for (x in results.data) {
+					myString += "<li>"+results.data[x][0]+"</li>";
+			}
+			changeText(myString);
+			console.log(myString);
+		}
+	});
+});
+
+function main(){
+	console.log("Loaded");
+}
+
+function changeText(text){
+	var elem = document.getElementById("whatsnewList");
+	elem.innerHTML = "<ul id='whatsnewList'>"+text+"</ul>";
+}
